ddef0199b613a0db10545fe3b7bed81fc62890c1
Àlex Garcia <agarciab@viewnext.com>
2019-06-19 09:11:28 +0200

    Microserveis de negoci

---
+++ limit-service/src/main/java/com/vn/msp/controller/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.vn.msp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vn.msp.model.LimitConfiguration;
+import com.vn.msp.service.Configuration;
+
+@RestController
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+}
